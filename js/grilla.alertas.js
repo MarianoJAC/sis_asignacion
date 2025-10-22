77f4458b8468bec65a31725f17112c5709923a6c
MJAC <marianojacirillo@gmail.com>
2025-10-22 20:49:14 -0300

    asad

--- js/grilla.alertas.js
+++ js/grilla.alertas.js
@@ -1,7 +1,7 @@
 
 export function mostrarMensaje(tipo = 'info', texto = '', opciones = {}) {
   Swal.fire({
-    icon: tipo, // 'success', 'error', 'warning', 'info'
+    icon: tipo === 'success' ? false : tipo, // Disable icon only for 'success' type
     text: texto,
     toast: true,
     timer: 3000,
